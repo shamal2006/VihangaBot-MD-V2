95ea5bd0ad5346e5b89fddb5b2fbaf2a3aec4af8
shamal2006 <111765741+shamal2006@users.noreply.github.com>
2022-08-22 20:39:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,15 +22,15 @@ global.OWNER_REACT = '🧑🏻‍💻' //මෙතනට emoji එකක දා�
 global.REACT_NUMBER = '9476342733' //මෙතන number එක.
 
 // Other
-global.owner = ['9476342733',''] //මෙතන number එක.
-global.premium = ['9476342733','9476342733'] //මෙතන number එක.
+global.owner = ['94769266251',''] //මෙතන number එක.
+global.premium = ['94769266251','94769266251'] //මෙතන number එක.
 global.botname = 'VIHANGA BOT INC' //මෙතන නම.
-global.ownername = 'Vihanga MD' //මෙතන නම.
-global.ownername = 'ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ' //මෙතන නම.
+global.ownername = 'Shamal MD' //මෙතන නම.
+global.ownername = 'SMT' //මෙතන නම.
 global.group1 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
 global.group2 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
-global.packname = 'VIHANGA BOT INC' //මෙතන නම.
-global.cap = '*By Vihanga MD*' //මෙතන නම.
+global.packname = 'shamal TS BOT INC' //මෙතන නම.
+global.cap = '*By ts MD*' //මෙතන නම.
 global.img = 'https://telegra.ph/file/88d6fdeff08bbc4945901.jpg' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
 global.ytchannel = 'https://youtu.be/Z9rIR0Vopb8' //මෙතන කැමති ලින්ක් එකක්.
 global.monayawal = '100000' //මෙතන මාරු කරන්න එපා.
